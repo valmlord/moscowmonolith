7a178473678f5f883b2f078f15c5dc32ee53b03e
Korshukov Stepan <korshukovstepan@gmail.com>
2021-05-10 19:52:33 +0300

    add to project

--- dist/script.js
+++ dist/script.js
@@ -1808,100 +1808,70 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-// HAMBURGER
-window.addEventListener("DOMContentLoaded", function () {
-  var menu = document.querySelector(".menu"),
-      menuItem = document.querySelectorAll(".menu__item"),
-      hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("hamburger_active");
-    menu.classList.toggle("menu_active");
+window.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu__item'),
+      hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
   });
   menuItem.forEach(function (item) {
-    item.addEventListener("click", function () {
-      hamburger.classList.toggle("hamburger_active");
-      menu.classList.toggle("menu_active");
+    item.addEventListener('click', function () {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
     });
   });
+}); //Modal
+
+$("[data-modal=consult]").on("click", function () {
+  $(".overlay, #consult").fadeIn("slow");
 });
+$(".modal__close").on("click", function () {
+  $(".overlay, #consult, #thanks").fadeOut("slow");
+}); //Sender
 
-(function ($) {
-  //Modal
-  $("[data-modal=consult]").on("click", function () {
-    $(".overlay, #consult").fadeIn("slow");
+$("form").submit(function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "sendform.php",
+    data: $(this).serialize()
+  }).done(function () {
+    $(this).find("input").val("");
+    $("#consult").fadeOut();
+    $(".overlay, #thanks").fadeIn("slow");
+    $("form").trigger("reset");
   });
-  $("[data-modal=call").on("click", function () {
-    $(".overlay, #call").fadeIn("slow");
-  });
-  $(".modal__close").on("click", function () {
-    $(".overlay, #consult, #thanks, #call").fadeOut("slow");
-  }); //Validator
+  return false;
+}); //Validator
 
-  function validateForms(form) {
-    $(form).validate({
-      rules: {
-        name: {
-          required: true,
-          minlength: 2
-        },
-        phone: "required",
-        email: {
-          required: true,
-          email: true
-        }
+function validateForms(form) {
+  $(form).validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
       },
-      messages: {
-        name: {
-          required: "Пожалуйста, введите свое имя",
-          minlength: jQuery.validator.format("Введите {0} символа!")
-        },
-        phone: "Пожалуйста введите свой номер телефона",
-        email: {
-          required: "Пожалуйста, введите свою почту",
-          email: "Неправильно введен адрес почты"
-        }
+      phone: "required",
+      email: {
+        required: true,
+        email: true
       }
-    });
-  }
-
-  validateForms("#consult-form");
-  validateForms("#consult form");
-  validateForms("#call form");
-  $("input[name=phone]").mask("8 (999) 999-99-99"); //Mailer
-
-  $("form").submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "sendform.php",
-      data: $(this).serialize()
-    }).done(function () {
-      $(this).find("input").val("");
-      $("#consult, #call").fadeOut();
-      $(".overlay, #thanks").fadeIn("slow");
-      $("form").trigger("reset");
-    });
-    return false;
-  }); //Smooth Scroll
-
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1000) {
-      $(".pageup").fadeIn();
-    } else {
-      $(".pageup").fadeOut();
+    },
+    messages: {
+      name: {
+        required: "Пожалуйста, введите свое имя",
+        minlength: jQuery.validator.format("Введите {0} символа!")
+      },
+      phone: "Пожалуйста введите свой номер телефона"
     }
   });
-  $("a[href^='#']").click(function () {
-    var _href = $(this).attr("href");
+}
 
-    $("html, body").animate({
-      scrollTop: $(_href).offset().top + "px"
-    });
-    return false;
-  }); // Animation
-
-  new WOW().init();
-})(jQuery);
+validateForms("#consult-form");
+validateForms("#consult form");
+$("input[name=phone]").mask("8 (999) 999-99-99");
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,109 +1,75 @@
-// HAMBURGER
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
 
-window.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".menu"),
-        menuItem = document.querySelectorAll(".menu__item"),
-        hamburger = document.querySelector(".hamburger");
-
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("hamburger_active");
-        menu.classList.toggle("menu_active");
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
     });
 
-    menuItem.forEach((item) => {
-        item.addEventListener("click", () => {
-            hamburger.classList.toggle("hamburger_active");
-            menu.classList.toggle("menu_active");
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
         });
     });
 });
 
-(function ($) {
+//Modal
 
-    //Modal
+$("[data-modal=consult]").on("click", function () {
+    $(".overlay, #consult").fadeIn("slow");
+});
+$(".modal__close").on("click", function () {
+    $(".overlay, #consult, #thanks").fadeOut("slow");
+});
 
-    $("[data-modal=consult]").on("click", function () {
-        $(".overlay, #consult").fadeIn("slow");
+//Sender
+
+$("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "sendform.php",
+        data: $(this).serialize(),
+    }).done(function () {
+        $(this).find("input").val("");
+        $("#consult").fadeOut();
+        $(".overlay, #thanks").fadeIn("slow");
+
+        $("form").trigger("reset");
     });
-    $("[data-modal=call").on("click", function () {
-        $(".overlay, #call").fadeIn("slow");
-    });
-    $(".modal__close").on("click", function () {
-        $(".overlay, #consult, #thanks, #call").fadeOut("slow");
-    });
+    return false;
+});
 
-    //Validator
 
-    function validateForms(form) {
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2,
-                },
-                phone: "required",
-                email: {
-                    required: true,
-                    email: true,
-                },
+//Validator
+
+function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
             },
-            messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символа!"),
-                },
-                phone: "Пожалуйста введите свой номер телефона",
-                email: {
-                    required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно введен адрес почты",
-                },
+            phone: "required",
+            email: {
+                required: true,
+                email: true,
             },
-        });
-    }
-
-    validateForms("#consult-form");
-    validateForms("#consult form");
-    validateForms("#call form");
-
-    $("input[name=phone]").mask("8 (999) 999-99-99");
-
-    //Mailer
-
-    $("form").submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "sendform.php",
-            data: $(this).serialize(),
-        }).done(function () {
-            $(this).find("input").val("");
-            $("#consult, #call").fadeOut();
-            $(".overlay, #thanks").fadeIn("slow");
-
-            $("form").trigger("reset");
-        });
-        return false;
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!"),
+            },
+            phone: "Пожалуйста введите свой номер телефона",
+        },
     });
+}
 
-    //Smooth Scroll
+validateForms("#consult-form");
+validateForms("#consult form");
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 1000) {
-            $(".pageup").fadeIn();
-        } else {
-            $(".pageup").fadeOut();
-        }
-    });
-
-    $("a[href^='#']").click(function () {
-        const _href = $(this).attr("href");
-        $("html, body").animate({
-            scrollTop: $(_href).offset().top + "px",
-        });
-        return false;
-    });
-
-    // Animation
-
-    new WOW().init();
-})(jQuery);
+$("input[name=phone]").mask("8 (999) 999-99-99");
